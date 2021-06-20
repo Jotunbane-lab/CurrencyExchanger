bd8055d9e7dceb23f393ade59216e30f1935e106
Jotunbane <PawMikolajczak@gmail.com>
2021-06-20 11:48:30 +0200

    Initial commit

---
+++ src/main/java/lab/jotunbane/CurrencyExchanger/CurrencyExchangerApplication.java
@@ -0,0 +1,14 @@
+package lab.jotunbane.CurrencyExchanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangerApplication.class, args);
+		System.out.println("App Started");
+	}
+
+}
