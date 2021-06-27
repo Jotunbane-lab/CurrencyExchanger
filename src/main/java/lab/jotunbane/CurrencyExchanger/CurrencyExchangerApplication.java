a4e329f045c8c3d33fd319538fd64d3c5b3eaddb
Jotunbane <PawMikolajczak@gmail.com>
2021-06-27 15:35:36 +0200

    Main Functionalities done.

--- src/main/java/lab/jotunbane/CurrencyExchanger/CurrencyExchangerApplication.java
+++ src/main/java/lab/jotunbane/CurrencyExchanger/CurrencyExchangerApplication.java
@@ -4,15 +4,19 @@ import controller.Controller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.math.BigDecimal;
+
 @SpringBootApplication
 public class CurrencyExchangerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyExchangerApplication.class, args);
 		System.out.println("App Started");
+//		Some quick manual testing
+//		Controller controller = new Controller();
+//		BigDecimal bigDecimal = controller.mainFunctionality("EUR", "USD", BigDecimal.valueOf(100));
+//		System.out.println(bigDecimal);
 
-		Controller controller = new Controller();
-		System.out.println(controller.getAsk("USD"));
 	}
 
 }
